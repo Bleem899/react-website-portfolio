02157901634f744326c57c9c129600cdd6fce97f
@Bleem899 <blmorris899@gmail.com>
2021-12-20 22:19:17 -0600

    All files added to repo.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/pages/Home'
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import ThisIsMe from './components/pages/ThisIsMe';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/about'  element={<About/>}/>
+          <Route path='/projects'  element={<Projects/>}/>
+          <Route path='/this-is-me'  element={<ThisIsMe/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
