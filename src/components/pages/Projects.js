37040cfc3197ee46fd11be38380bd666bf17d6a5
@Bleem899 <blmorris899@gmail.com>
2021-12-21 22:06:23 -0600

    Began work on content of projects page.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import '../../App.css'
+import { GitHubButton } from "../GitHubButton"
+import '../HeroSection.css'
+
 
 export default function Projects() {
-    return <h1 className="projects">PROJECTS</h1>
+    return (
+        <>
+            <h1 className="projects">PROJECTS</h1>
+            <div className="this-is-me-paragraph">
+                <h1>What you need to know about the rest of me:</h1>
+                <div className="btn-container">
+                    <div className="hero-btns">
+                        <GitHubButton
+                            className="btns"
+                            buttonStyle="btn--outlines"
+                            buttonSize="btn--larges"
+                        >
+                            Check out my GitHub
+                        </GitHubButton>
+                    </div>
+                </div>
+                <section className="this-is-me-section">
+                    <h2>this one</h2>
+                    <p>
+                        project 1
+                    </p>
+                    <p>
+                        project description
+                    </p>
+                </section>
+            </div>
+        </>
+    )
 }
