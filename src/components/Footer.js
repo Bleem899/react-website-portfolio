09aa1cd40d0c99aa94e6be218f7a09dc7e27626c
@Bleem899 <blmorris899@gmail.com>
2022-01-06 22:57:02 -0600

    Added github icon and links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ContactForm from './ContactForm'
 import './Footer.css'
+import Linking from 'react'
 
 function Footer() {
     return (
@@ -20,9 +21,15 @@ function Footer() {
             <section className="social-media">
                 <div className="social-media-wrap">
                     <div className="social-icons">
-                        <Link to="/" className="social-logo">blmorris899@gmail.com</Link>
-                        <Link to="/" className='social-icon-link linkedin' target="_blank" aria-label="LinkedIn">
+                        <Link to='#' className="social-logo" onClick={(e) => {
+                            window.location = "mailto:blmorris899@gmail.com"
+                            e.preventDefault()
+                        }}>blmorris899@gmail.com</Link>
+                        <Link to="/https://www.linkedin.com/in/benjamin-lee-morris/" className='social-icon-link linkedin' target="_blank" aria-label="LinkedIn">
                             <i className="fab fa-linkedin"></i> 
+                        </Link>
+                        <Link to="/https://github.com/Bleem899?tab=repositories" className='social-icon-link github' target="_blank" aria-label="GitHub">
+                            <i className="fab fa-github"></i> 
                         </Link>
                     </div>
                 </div>
