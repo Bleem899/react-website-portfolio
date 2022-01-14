6c33784aef8aded7cca610a3a0b1240c0e41d7be
@Bleem899 <blmorris899@gmail.com>
2022-01-13 22:21:25 -0600

    Removed cards to add link.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,20 @@
 import '../../App.css'
 import HeroSection from '../HeroSection'
-import React from 'react'
-import Cards from '../Cards'
+import React, { useEffect } from 'react'
 import Footer from '../Footer'
+import { Link } from 'react-router-dom'
 
 
 function Home () {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
     return (
         <>
             <HeroSection/>
-            <Cards/>
+            <div className="about-paragraph">
+                <h2>Check out my <Link target="_blank" to={"//docs.google.com/document/d/1jPYt3QbmW20_IHNehR9Cn_wo1qJy9sFdw8SXoFXv9i4/edit?usp=sharing"}>RESUME</Link> if you haven't seen it yet</h2>
+            </div>
             <Footer/>
         </>
     )
